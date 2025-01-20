bc400d5b1be72a283847ef15046a957bbb41a1e8
Clansty <i@gao4.pw>
2025-01-21 00:03:26 +0800

    set FileVersion to git version

--- AquaMai/Properties/AssemblyInfo.cs
+++ AquaMai/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using MelonLoader;
 [assembly: AssemblyCopyright("Created by " + AquaMai.BuildInfo.Author)]
 [assembly: AssemblyTrademark(AquaMai.BuildInfo.Company)]
 [assembly: AssemblyVersion(AquaMai.BuildInfo.Version)]
-[assembly: AssemblyFileVersion(AquaMai.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(AquaMai.BuildInfo.GitVersion)]
 [assembly: MelonInfo(typeof(AquaMai.AquaMai), AquaMai.BuildInfo.Name, AquaMai.BuildInfo.GitVersion, AquaMai.BuildInfo.Author, AquaMai.BuildInfo.DownloadLink)]
 [assembly: MelonColor()]
 [assembly: HarmonyDontPatchAll]
