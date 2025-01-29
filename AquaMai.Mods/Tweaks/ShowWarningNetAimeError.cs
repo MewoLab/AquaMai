f489428a3201c2f6615cc467bd34973861dfe996
zaphkito <zaphkito@outlook.com>
2025-01-29 21:25:37 +0800

    [+] ShowWarningNetAimeError && delete IgnoreAimeServerError

---
+++ AquaMai.Mods/Tweaks/ShowWarningNetAimeError.cs
@@ -0,0 +1,29 @@
+using AquaMai.Config.Attributes;
+using HarmonyLib;
+using Manager;
+
+namespace AquaMai.Mods.Tweaks;
+
+[ConfigSection(
+    en: "Show warning network icon if aime server has issue.",
+    zh: "如果网络正常，但是Aime报错，则显示黄网，建议开启")]
+public class ShowWarningNetAimeError
+{
+    [HarmonyPatch(typeof(OperationManager), "NetIconStatus", MethodType.Getter)]
+    [HarmonyPrefix]
+    public static bool Prefix(ref bool __result)
+    {
+        if (!this.IsAliveServer || !this.IsAliveAime)
+        {
+            if (!this.WasDownloadSuccessOnce)
+            {
+                __result = OperationManager.NetStatus.Offline;
+                return false;
+            }
+            __result = OperationManager.NetStatus.LimitedOnline;
+            return false;
+        }
+        __result = OperationManager.NetStatus.Online;
+        return false;
+    }
+}
