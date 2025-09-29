5112349b7bad03c907b2b7e7ec616a297bd3cea4
Minepig <30530115+Minepig@users.noreply.github.com>
2025-09-30 04:14:58 +0800

    add feature
    Sync 1P headphone volume to game master volume

--- AquaMai.Mods/GameSystem/Sound.cs
+++ AquaMai.Mods/GameSystem/Sound.cs
@@ -2,6 +2,7 @@ using AquaMai.Config.Attributes;
 using HarmonyLib;
 using Manager;
 using System;
+using MelonLoader;
 
 namespace AquaMai.Mods.GameSystem;
 
@@ -19,6 +20,17 @@ public static class Sound
         zh: "是否启用八声道",
         en: "Enable 8-Channel")]
     private readonly static bool enable8Channel = false;
+    
+    [ConfigEntry(
+        zh: """
+            将1P耳机音量同步给游戏主音量（外放音量）
+            注意：Maimai处理耳机音量的方式未知，若此功能与八声道同时开启，可能导致耳机音量被缩放两次，推荐在仅使用1P且不开启八声道时使用,
+            """,
+        en: """
+            Sync 1P headphone volume to game master volume
+            Maimai's headphone volume handling is unclear. Enabling with 8ch may double-scale headphone volume. It is recommended to use this only in 1P without 8ch.
+            """)]
+    private readonly static bool sync1pVolumeToMaster = false;
 
     [ConfigEntry(
         en: "Music Volume.",
@@ -81,4 +93,67 @@ public static class Sound
             volume = musicVolume;
         }
     }
+    
+    /*
+     * 将1P耳机音量同步给游戏主音量（外放音量）
+     * Sync 1P headphone volume to game master volume
+     *
+     * 注意：Maimai处理耳机音量的方式未知，若此功能与八声道同时开启，可能导致耳机音量被缩放两次，
+     *       推荐在仅使用1P且不开启八声道时使用
+     * Note: Maimai's headphone volume handling is unclear. Enabling with 8ch may double-scale headphone volume.
+     *       It is recommended to use this only in 1P without 8ch.
+     */
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(SoundCtrl), nameof(SoundCtrl.Initialize))]
+    public static void SoundCtrl_Initialize_Prefix(SoundCtrl __instance)
+    {
+        __instance._masterVolume = 0.05f; // Default headphone volume
+        // MelonLogger.Msg("master volume initialized to 0.05");
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(SoundCtrl), nameof(SoundCtrl.SetMasterVolume))]
+    public static void SoundCtrl_SetMasterVolume_Postfix(SoundCtrl __instance, float[] ____headPhoneVolume, float volume)
+    {
+        __instance._masterVolume = __instance.Adjust0_1(volume * ____headPhoneVolume[0]);
+        // MelonLogger.Msg($"master volume set to {__instance._masterVolume}");
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(SoundCtrl), nameof(SoundCtrl.ResetMasterVolume))]
+    public static void SoundCtrl_ResetMasterVolume_Postfix(SoundCtrl __instance, float[] ____headPhoneVolume)
+    {
+        __instance._masterVolume = __instance.Adjust0_1(____headPhoneVolume[0]);
+        // MelonLogger.Msg($"master volume reset to {__instance._masterVolume}");
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(SoundCtrl), nameof(SoundCtrl.SetHeadPhoneVolume))]
+    public static void SoundCtrl_SetHeadPhoneVolume_Postfix(SoundCtrl __instance, int targerID, float volume)
+    {
+        // MelonLogger.Msg($"setting headphone volume : target {targerID}, volume {volume}");
+        if (targerID == 0)
+        {
+            __instance._masterVolume = __instance.Adjust0_1(volume);
+            // MelonLogger.Msg($"master volume set to {__instance._masterVolume}");
+            // fixme)) 由于每个播放通道的master音量只在切换音频文件时重设，目前调节耳机音量不能及时反映到master
+            // 需要在这里加一段调整所有通道音量的代码，但这样就需要记录开始播放时的volume参数
+        }
+    }
+    
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(SoundCtrl), nameof(SoundCtrl.Play))]
+    public static void SoundCtrl_Play_Prefix(SoundCtrl __instance, SoundCtrl.PlaySetting setting)
+    {
+        // fixme)) 在这里记录每个文件开始播放时的volume参数
+        if (setting.PlayerID != 3 && setting.PlayerID != 4 && setting.PlayerID != 5)
+        {
+            return;
+        }
+        // PlayerID 3 ~ 5 are not controlled by master volume, so we need a extra scaling
+        // MelonLogger.Msg($"scaling volume: {__instance._masterVolume}");
+        setting.Volume *= __instance._masterVolume;
+    }
+    
+    
 }
