a28e4e50db0cf00f5b27f221478c5312dcf65ec6
凌莞~(=^▽^=) <opensource@c5y.moe>
2025-10-24 00:02:36 +0800

    Update AquaMai.Mods/GameSystem/Assets/AssetBundleManager.cs

--- AquaMai.Mods/GameSystem/Assets/AssetBundleManager.cs
+++ AquaMai.Mods/GameSystem/Assets/AssetBundleManager.cs
@@ -5,6 +5,7 @@ using AquaMai.Config.Attributes;
 namespace AquaMai.Mods.GameSystem.Assets;
 
 [ConfigSection(
+	defaultOn: true,
     en: "Ignore .ab image resources that fail to load",
     zh: "忽略加载失败的 .ab 图片资源")]
 public class IgnoreLoadFailedAssetBundle
