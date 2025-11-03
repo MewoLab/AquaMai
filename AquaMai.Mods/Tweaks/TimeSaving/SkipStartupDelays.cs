10902c493628a136d4e92386808eb01aca17549d
Rain_Effect <admin@mc-rain.cn>
2025-11-03 08:45:51 +0800

    [+] 摄像头/网络检查跳过等待

--- AquaMai.Mods/Tweaks/TimeSaving/SkipStartupDelays.cs
+++ AquaMai.Mods/Tweaks/TimeSaving/SkipStartupDelays.cs
@@ -15,6 +15,24 @@ namespace AquaMai.Mods.Tweaks.TimeSaving;
     defaultOn: true)]
 public class SkipStartupDelays
 {
+    [ConfigEntry(
+        name: "跳过摄像头检查",
+        en: "Skip camera check to speed up the game boost process.",
+        zh: """
+            在自检界面，需要等待摄像头彻底检查完毕后才能进入
+            开了这个会将检查过程丢入后台进行
+            """)]
+    private static readonly bool skipStartupCameraCheck = false;
+
+    [ConfigEntry(
+        name: "跳过启动网络检查",
+        en: "Skip network check to speed up the game boost process.",
+        zh: """
+            在自检界面，需要等待网络彻底检查完毕后才能进入
+            开了这个会将检查过程丢入后台进行
+            """)]
+    private static readonly bool skipPowerOnNetworkCheck = false;
+
     [HarmonyPrefix]
     [HarmonyPatch(typeof(PowerOnProcess), "OnStart")]
     public static void PrePowerOnStart(ref float ____waitTime)
@@ -26,9 +44,23 @@ public class SkipStartupDelays
     [HarmonyPatch(typeof(StartupProcess), "OnUpdate")]
     public static void PreStartupUpdate(byte ____state, ref Stopwatch ___timer)
     {
-        if (____state == 8)
+        if (skipStartupCameraCheck && ____state == 2)
+        {
+            ____state = 3;
+        }
+        else if (____state == 8)
         {
             Traverse.Create(___timer).Field("elapsed").SetValue(2 * 10000000L);
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PowerOnProcess), "OnUpdate")]
+    public static void PreStartupUpdate(ref byte ____state)
+    {
+        if (skipPowerOnNetworkCheck && ____state == 6)
+        {
+            ____state = 7;
         }
     }
 }
